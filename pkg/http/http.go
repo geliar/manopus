751febe041ed2fe13512059ff0ad7f2feb2105e2
Dmytro Lahoza <dlahoza@gdfsi.com>
2019-01-08 23:12:18 +0200

    HTTP server: handlers manipulation

--- pkg/http/http.go
+++ pkg/http/http.go
@@ -6,15 +6,22 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/rs/zerolog/hlog"
+
+	"github.com/DLag/midsimple"
+
 	"github.com/geliar/manopus/pkg/log"
 )
 
 type HTTPServer struct {
-	config   HTTPConfig
-	instance *http.Server
-	routes   map[string]http.Handler
+	config       HTTPConfig
+	instance     *http.Server
+	routes       map[string]http.Handler
+	defaultRoute http.Handler
 	sync.RWMutex
 }
+
+var server HTTPServer
 
 func Init(ctx context.Context, config HTTPConfig) *HTTPServer {
 	if config.Listen == "" {
@@ -22,17 +29,29 @@ func Init(ctx context.Context, config HTTPConfig) *HTTPServer {
 		return nil
 	}
 
-	server := new(HTTPServer)
 	server.config = config
 	server.Start(ctx)
-	return server
+	return &server
+}
+
+func AddHandler(ctx context.Context, path string, h http.Handler) {
+	server.AddHandler(ctx, path, h)
+}
+
+func SetDefaultHandler(ctx context.Context, h http.Handler) {
+	server.SetDefaultHandler(ctx, h)
 }
 
 func (s *HTTPServer) Start(ctx context.Context) {
 	l := logger(ctx)
+	handler := midsimple.New(hlog.NewHandler(l)).
+		Use(hlog.RemoteAddrHandler("ip")).
+		Use(hlog.UserAgentHandler("user_agent")).
+		Use(hlog.RefererHandler("referer")).
+		WrapFunc(s.routerHandler)
 	s.instance = &http.Server{
 		Addr:    s.config.Listen,
-		Handler: http.HandlerFunc(s.routerHandler),
+		Handler: handler,
 	}
 	go func() {
 		l.Info().Msgf("Listening HTTP requests on %s", s.config.Listen)
@@ -55,13 +74,38 @@ func (s *HTTPServer) Stop(ctx context.Context) {
 	}
 }
 
+func (s *HTTPServer) AddHandler(ctx context.Context, path string, h http.Handler) {
+	l := logger(ctx).With().Str("http_path", path).Logger()
+	s.Lock()
+	defer s.Unlock()
+	if _, ok := s.routes[path]; ok {
+		l.Error().Msg("Trying to add HTTP handler for existing route")
+		return
+	}
+	s.routes[path] = h
+	l.Debug().Msg("Added HTTP server handler")
+}
+
+func (s *HTTPServer) SetDefaultHandler(ctx context.Context, h http.Handler) {
+	l := logger(ctx)
+	s.Lock()
+	defer s.Unlock()
+	s.defaultRoute = h
+	l.Debug().Msg("Set default HTTP server handler")
+}
+
 func (s *HTTPServer) routerHandler(w http.ResponseWriter, r *http.Request) {
+	hlog.FromRequest(r).Debug().
+		Msgf("%s %s", r.Method, r.RequestURI)
 	s.RLock()
 	defer s.RUnlock()
 	for k, v := range s.routes {
-		if strings.Contains(r.RequestURI, k) {
+		if strings.HasPrefix(r.RequestURI, k) {
 			v.ServeHTTP(w, r)
 			return
 		}
+	}
+	if s.defaultRoute != nil {
+		s.defaultRoute.ServeHTTP(w, r)
 	}
 }
